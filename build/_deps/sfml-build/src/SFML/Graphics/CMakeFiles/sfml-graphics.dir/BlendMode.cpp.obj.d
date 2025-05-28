ed9d97dbff08c1236e8054babc5c5109b36b79bc
Tomás <tomivignau@gmail.com>
2025-05-28 09:18:06 -0300

    Agregado Pantalla Inicio

---
+++ build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj.d
@@ -0,0 +1,5 @@
+_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj: \
+ E:\ProyectoTallerMultimedial\build\_deps\sfml-src\src\SFML\Graphics\BlendMode.cpp \
+ E:/ProyectoTallerMultimedial/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
+ E:/ProyectoTallerMultimedial/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
+ E:/ProyectoTallerMultimedial/build/_deps/sfml-src/include/SFML/Config.hpp
